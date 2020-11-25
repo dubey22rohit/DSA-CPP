51b4039a824fe91b0d2ab42790d95a7c833c5686
Rohit Dubey <dubey22rohit@gmail.com>
2020-11-25 21:16:38 +0530

    Second Commit

---
+++ Searching And Sorting/SelectionSort.cpp
@@ -0,0 +1,23 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+void selectionSort(int a[],int n){
+		for(int i = 0;i<n-1;i++){
+			int minIndex = i;
+			for(int j = i+1;j<n;j++){
+				if(a[j] < a[minIndex]){
+					minIndex = j;
+				}
+			}
+			swap(a[i],a[minIndex]);
+		}
+}
+int main(){
+int arr[] = {5,1,7,9,3,4};
+int n = sizeof(arr)/sizeof(arr[0]);
+selectionSort(arr,n);
+
+for(int i = 0;i<n;i++){
+	cout<<arr[i];
+}
+}

cac9ace06e776a9be69f0466ea1ca0f417b98d4c
Rohit Dubey <dubey22rohit@gmail.com>
2020-11-26 20:43:31 +0530

    Wave sort fixed

--- Searching And Sorting/WaveSort.cpp
+++ Searching And Sorting/WaveSort.cpp
@@ -1,11 +1,11 @@
-#include<iostream>
+#include<bits/stdc++.h>
 using namespace std;
 void waveSort(int a[],int n){
 for(int i = 0;i<n;i+=2){
     if(i > 0 && a[i-1] > a[i]){
         swap(a[i],a[i-1]);
     }
-    if(i > n-1 && a[i+1] > a[i]){
+    if(i < n-1 && a[i+1] > a[i]){
         swap(a[i],a[i+1]);
     }
 }
